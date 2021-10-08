9eb5fbd62c4a2b87b0d9a961ed02044f2199eeb0
doremichin <johnkim994@naver.com>
2021-10-09 01:35:15 +0900

    [UN-5] Search API 적용 및 로직 개발, [UN-8] Main PhotosList API 로직 개발

--- src/views/main/containers/MainContainer.js
+++ src/views/main/containers/MainContainer.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import styled from 'styled-components'
 import Visual from "../components/Visual";
+import axios from "axios";
+import {useDispatch, useSelector} from "react-redux";
+import {Action} from "../../../redux/main/slice";
+import GridList from "../../shared/components/List/GridList";
+import PhotoItem from "../../shared/components/Item/PhotoItem";
 
 const MainContainer = () => {
+
+    const dispatch = useDispatch();
+    const {photos} = useSelector(state => state.main);
+    console.log(photos)
+
+    const getPhotos = async () => {
+        const result = await axios({
+            url : `https://api.unsplash.com/photos`,
+            method : 'get',
+            headers : {
+                Authorization: 'Client-ID 6_2N9-xx9qq8gNRcyVQgQmNVMmbSRuaIqMc1KQYpwYA'
+            },
+            params : {
+                page : 1,
+                per_page : 10,
+            }
+        })
+        console.log(result)
+        dispatch(Action.Creators.setPhotos(result.data))
+    }
+
+    useEffect(() => {
+        getPhotos();
+    },[])
+    const renderItem = (item) => <PhotoItem item={item}/>
     return(
         <Container>
             <Visual/>
+            <GridList data={photos} renderItem={renderItem}/>
         </Container>
     )
 };
--- src/views/search/containers/SearchContainer.js
+++ src/views/search/containers/SearchContainer.js
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import styled from 'styled-components'
+import axios from "axios";
+import {useParams} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+import {Action} from "../../../redux/search/slice";
+import GridList from "../../shared/components/List/GridList";
+import PhotoItem from "../../shared/components/Item/PhotoItem";
 
 const SearchContainer = () => {
+
+    const {query} = useParams();
+    const dispatch = useDispatch();
+    const {photos} = useSelector(state => state.search);
+
+    const searchPhotos = async () => {
+        const result = await axios({
+            url : `https://api.unsplash.com/search/`,
+            method : 'get',
+            headers : {
+                Authorization: 'Client-ID 6_2N9-xx9qq8gNRcyVQgQmNVMmbSRuaIqMc1KQYpwYA'
+            },
+            params : {
+                page : 1,
+                query
+            }
+        })
+        dispatch(Action.Creators.setSearchResults(result.data))
+    }
+    useEffect(() => {
+        searchPhotos();
+    },[query])
+
+    const renderItem = (item) => <PhotoItem item={item}/>
+    console.log(photos)
     return(
         <Container>
-            SearchContainer
+            검색결과 {photos.total}개가 검색됨
+            <GridList
+                data={photos.results}
+                renderItem={renderItem}
+            />
         </Container>
     )
 };
