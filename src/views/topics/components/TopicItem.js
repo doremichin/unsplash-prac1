ab60409aafa22ddc716a98e8a57839e01b9517af
doremichin <johnkim994@naver.com>
2021-10-19 15:30:43 +0900

    [UN-10] Topicspage, TopicDetailpage 연결, 스타일 수정, TopicDetailpage/StatusBox 로직 개발

--- src/views/topics/components/TopicItem.js
+++ src/views/topics/components/TopicItem.js
@@ -1,43 +1,63 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import { Link } from 'react-router-dom';
+
 import StatusButton from '../../shared/components/Button/StatusButton';
+import { IconContributions } from '../../../icons';
 
-const TopicItem = ({ item }) => (
-  <Container>
-    <StatusButton item={item} />
-    <Thumb>
-      <img src={item.cover_photo.urls.regular} alt={item.cover_photo.alt_description} />
-    </Thumb>
-    <Desc>
-      <Title>
-        <div>
-          <h3>{item.title}</h3>
+const TopicItem = ({ item }) => {
+  const handlePhotosCount = () => {
+    const count = item.total_photos;
+    if (count >= 1000) {
+      return (`${(Math.floor(count / 100)) / 10}k`);
+    }
+    return count;
+  };
+  return (
+    <Container to={`/topics/${item.slug}`}>
+      <StatusButton item={item} />
+      <Thumb>
+        <img src={item.cover_photo.urls.regular} alt={item.cover_photo.alt_description} />
+      </Thumb>
+      <Desc>
+        <Title>
+          <div>
+            <h3>{item.title}</h3>
+            <p>
+              by&nbsp;
+              <a href={item.owners[0].portfolio_url}>
+                {item.owners[0].name}
+              </a>
+            </p>
+          </div>
+          <Owners>
+            <img src={item.owners[0].profile_image.large} alt="" />
+          </Owners>
+        </Title>
+        <Info dangerouslySetInnerHTML={{ __html: item.description }} />
+        <Contribution>
+          <IconContributions />
           <p>
-            by&nbsp;
-            <a href={item.owners[0].portfolio_url}>
-              {item.owners[0].name}
-            </a>
+            {handlePhotosCount()}
+            {' '}
+            contribution
           </p>
-        </div>
-        <Owners>
-          <img src={item.owners[0].profile_image.large} alt="" />
-        </Owners>
-      </Title>
-      <Info dangerouslySetInnerHTML={{ __html: item.description }} />
-      <Contribution>
-        dsad
-      </Contribution>
-    </Desc>
-  </Container>
-);
+        </Contribution>
+      </Desc>
+    </Container>
+  );
+};
 
-const Container = styled.div`
+const Container = styled(Link)`
+  display: block;
   border: 1px solid #eee;
   border-radius: 5px;
   overflow: hidden;
   position: relative;
   box-shadow: 0 4px 24px rgba(0, 0, 0, 0.04);
+  transition: 0.2s;
+  cursor: pointer;
   &:hover{
     border: 1px solid #d1d1d1;
   }
@@ -62,6 +82,9 @@ const Thumb = styled.div`
 const Desc = styled.div`
   height: 230px;
   padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
   a{
     color: #767676;
   }
@@ -70,12 +93,18 @@ const Title = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  margin-bottom: 15px;
   h3{
     font-weight: 900;
   }
   p{
     color: #767676;
+  }
+  a{
+    transition: 0.3s;
+    &:hover{
+      text-decoration: underline;
+      color: #111;
+    }
   }
 `;
 const Owners = styled.div`
@@ -91,7 +120,16 @@ const Info = styled.div`
   overflow: hidden;
 `;
 const Contribution = styled.div`
-  
+  display: flex;
+  align-items: center;
+  font-size: 14px;
+  font-weight: 600;
+  svg{
+    fill: #d1d1d1;
+    width: 18px;
+    margin-right: 10px;
+  }
+
 `;
 
 export default TopicItem;
