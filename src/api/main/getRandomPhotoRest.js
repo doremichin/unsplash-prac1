27e3515c6be7060fd14ce6031762bea25201f57d
doremichin <johnkim994@naver.com>
2021-10-30 18:51:35 +0900

    [API] 기존의 headers 인증 삭제, params를 이용해 인증

--- src/api/main/getRandomPhotoRest.js
+++ src/api/main/getRandomPhotoRest.js
@@ -5,9 +5,6 @@ export const getRandomPhotoRest = async (params) => {
     const { data } = await axios({
       url: 'https://api.unsplash.com/photos/random',
       method: 'get',
-      headers: {
-        Authorization: 'Client-ID 6_2N9-xx9qq8gNRcyVQgQmNVMmbSRuaIqMc1KQYpwYA',
-      },
       params,
     });
     return data;
--- src/api/search/searchPhotosRest.js
+++ src/api/search/searchPhotosRest.js
@@ -5,9 +5,6 @@ export const searchPhotosRest = async (params) => {
     const { data } = await axios({
       url: 'https://api.unsplash.com/search',
       method: 'get',
-      headers: {
-        Authorization: 'Client-ID 6_2N9-xx9qq8gNRcyVQgQmNVMmbSRuaIqMc1KQYpwYA',
-      },
       params,
     });
     return data;
