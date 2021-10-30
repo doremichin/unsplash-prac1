204cdbe5a3aa360d5c1c4f367c21a2cad7fd88ce
doremichin <johnkim994@naver.com>
2021-10-30 20:18:24 +0900

    [main, visual] 코드 정리..

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,9 +2,11 @@ import React from 'react';
 import styled from 'styled-components';
 
 import MainContainer from '../views/main/containers/MainContainer';
+import VisualContainer from '../views/main/containers/VisualContainer';
 
 const Main = () => (
   <Container>
+    <VisualContainer />
     <MainContainer />
   </Container>
 );
--- src/views/main/containers/MainContainer.js
+++ src/views/main/containers/MainContainer.js
@@ -2,16 +2,14 @@ import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import { useDispatch, useSelector } from 'react-redux';
 
-import Visual from '../components/Visual';
-import { Action } from '../../../redux/main/slice';
+import { Action } from '../../../redux/photos/slice';
 import PhotoItem from '../../shared/components/Item/PhotoItem';
 import { ContentContainer } from '../../shared/components/Layout/Layout.Styled';
 import MainPhotoList from '../../shared/components/List/MainPhotoList';
 
 const MainContainer = () => {
   const dispatch = useDispatch();
-  const { photos } = useSelector((state) => state.main);
-  const { randomPhoto } = useSelector((state) => state.main);
+  const { list } = useSelector((state) => state.photos);
 
   const getPhotos = () => {
     dispatch(Action.Creators.getPhotos({
@@ -20,22 +18,14 @@ const MainContainer = () => {
       client_id: '6_2N9-xx9qq8gNRcyVQgQmNVMmbSRuaIqMc1KQYpwYA',
     }));
   };
-  const getRandomPhoto = () => {
-    dispatch(Action.Creators.getRandomPhoto({
-      query: 'desktop wallpapers',
-      client_id: '6_2N9-xx9qq8gNRcyVQgQmNVMmbSRuaIqMc1KQYpwYA',
-    }));
-  };
   useEffect(() => {
     getPhotos();
-    getRandomPhoto();
   }, []);
   const renderItem = (item) => <PhotoItem item={item} />;
   return (
     <Container>
-      <Visual data={randomPhoto} />
       <ContentContainer>
-        <MainPhotoList data={photos} renderItem={renderItem} />
+        <MainPhotoList data={list} renderItem={renderItem} />
       </ContentContainer>
     </Container>
   );
