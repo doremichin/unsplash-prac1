6d8a17d9ed0317442643324c384c5936471df20b
doremichin <johnkim994@naver.com>
2021-10-30 20:27:20 +0900

    [main] infinite scroll 로직 개발

--- src/views/main/containers/MainContainer.js
+++ src/views/main/containers/MainContainer.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -6,26 +6,35 @@ import { Action } from '../../../redux/photos/slice';
 import PhotoItem from '../../shared/components/Item/PhotoItem';
 import { ContentContainer } from '../../shared/components/Layout/Layout.Styled';
 import MainPhotoList from '../../shared/components/List/MainPhotoList';
+import InfiniteScroll from '../../shared/components/InfiniteScroll';
 
 const MainContainer = () => {
   const dispatch = useDispatch();
   const { list } = useSelector((state) => state.photos);
+  const [page, setPage] = useState(1);
 
   const getPhotos = () => {
     dispatch(Action.Creators.getPhotos({
-      page: 1,
+      page,
       per_page: 15,
       client_id: '6_2N9-xx9qq8gNRcyVQgQmNVMmbSRuaIqMc1KQYpwYA',
     }));
   };
   useEffect(() => {
     getPhotos();
-  }, []);
+  }, [page]);
   const renderItem = (item) => <PhotoItem item={item} />;
+  const next = () => {
+    if (list.length) {
+      setPage((p) => p + 1);
+    }
+  };
   return (
     <Container>
       <ContentContainer>
-        <MainPhotoList data={list} renderItem={renderItem} />
+        <InfiniteScroll next={next}>
+          <MainPhotoList data={list} renderItem={renderItem} />
+        </InfiniteScroll>
       </ContentContainer>
     </Container>
   );
