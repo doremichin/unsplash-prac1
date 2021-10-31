b026117c9711200715d271fe4873505fbdf0945c
doremichin <johnkim994@naver.com>
2021-10-31 19:08:30 +0900

    [search] InfiniteScroll 적용

--- src/views/search/containers/SearchContainer.js
+++ src/views/search/containers/SearchContainer.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { Route, useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
@@ -6,14 +6,16 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Action } from '../../../redux/search/slice';
 import { ContentContainer } from '../../shared/components/Layout/Layout.Styled';
 import RelatedSearchesMenu from '../components/RelatedSearchesMenu';
-import SearchPhotos from '../components/SearchPhotos';
 import SearchCollections from '../components/SearchCollections';
 import SearchUsers from '../components/SearchUsers';
 import { ACCESS_KEY } from '../../../const/config';
+import SearchPhotos from '../components/SearchPhotos';
+import InfiniteScroll from '../../shared/components/InfiniteScroll';
 
 const SearchContainer = () => {
   const { query } = useParams();
   const dispatch = useDispatch();
+  const [page, setPage] = useState(1);
   const {
     photos, collections, users, related_searches,
   } = useSelector((state) => state.search);
@@ -27,9 +29,31 @@ const SearchContainer = () => {
     }));
   };
 
+  const gatherSearchPhotos = () => {
+    dispatch(Action.Creators.getNextSearchPhotos({
+      query,
+      page,
+      per_page: 15,
+      client_id: ACCESS_KEY,
+    }));
+  };
+
+  useEffect(() => {
+    if (page > 1) {
+      gatherSearchPhotos();
+    }
+  }, [page]);
+
   useEffect(() => {
     searchPhotos();
+    setPage(1);
   }, [query]);
+
+  const next = () => {
+    if (photos.results.length) {
+      setPage((p) => p + 1);
+    }
+  };
 
   if (!photos) return '...loading';
 
@@ -39,18 +63,19 @@ const SearchContainer = () => {
         <PageTitle>{query}</PageTitle>
 
         <RelatedSearchesMenu data={related_searches} />
+        <InfiniteScroll next={next}>
+          <Route path={['/search/photos/:query']}>
+            <SearchPhotos data={photos?.results} />
+          </Route>
 
-        <Route path={['/search/photos/:query']}>
-          <SearchPhotos data={photos?.results} />
-        </Route>
+          <Route path={['/search/collections/:query']}>
+            <SearchCollections data={collections?.results} />
+          </Route>
 
-        <Route path={['/search/collections/:query']}>
-          <SearchCollections data={collections?.results} />
-        </Route>
-
-        <Route path={['/search/users/:query']}>
-          <SearchUsers data={users?.results} />
-        </Route>
+          <Route path={['/search/users/:query']}>
+            <SearchUsers data={users?.results} />
+          </Route>
+        </InfiniteScroll>
 
       </ContentContainer>
 
